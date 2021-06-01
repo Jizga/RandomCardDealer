b9de5981c996121502b310742d9db2401997bdc8
Jizga <jizga93@gmail.com>
2021-06-01 17:25:27 +0000

    The Breathe Code's exercise done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const cardsIcons = [
+  `<i class="bi bi-suit-spade-fill"></i>`,
+  `<i class="bi bi-suit-club-fill"></i>`,
+  `<i class="bi bi-suit-diamond-fill"></i>`,
+  `<i class="bi bi-suit-heart-fill"></i>`,
+  `<i class="bi bi-suit-spade-fill text-danger"></i>`,
+  `<i class="bi bi-suit-club-fill text-danger"></i>`,
+  `<i class="bi bi-suit-diamond-fill text-danger"></i>`,
+  `<i class="bi bi-suit-heart-fill text-danger"></i>`
+];
+
+const cardValue = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "K", "Q", "J", "A"];
+
+const iconTopLefth = document.querySelector(".iconTopLefth");
+const iconBottomRigth = document.querySelector(".iconBottomRigth");
+const cardValueHtml = document.querySelector(".cardValue");
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //*** repetir el mismo icono */
+  iconTopLefth.innerHTML = iconBottomRigth.innerHTML = randomIcon(cardsIcons);
+  cardValueHtml.innerHTML = randomCardValue(cardValue);
+};
+
+const randomIcon = arr => {
+  let randomNumber = Math.floor(Math.random() * arr.length);
+  return arr[randomNumber];
+};
+
+const randomCardValue = arr => {
+  let randomNumber = Math.floor(Math.random() * arr.length);
+  return arr[randomNumber];
 };
