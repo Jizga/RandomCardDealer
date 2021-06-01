c335b10abe611182509a10f63a98a72bcbb3010a
Jizga <jizga93@gmail.com>
2021-06-01 17:50:36 +0000

    Fixed the change between red icons and black icons. Also, added the getRandomIntInclusive

--- src/app.js
+++ src/app.js
@@ -4,14 +4,10 @@ import "./style.css";
 import "./assets/img/4geeks.ico";
 
 const cardsIcons = [
-  `<i class="bi bi-suit-spade-fill"></i>`,
-  `<i class="bi bi-suit-club-fill"></i>`,
-  `<i class="bi bi-suit-diamond-fill"></i>`,
-  `<i class="bi bi-suit-heart-fill"></i>`,
-  `<i class="bi bi-suit-spade-fill text-danger"></i>`,
-  `<i class="bi bi-suit-club-fill text-danger"></i>`,
-  `<i class="bi bi-suit-diamond-fill text-danger"></i>`,
-  `<i class="bi bi-suit-heart-fill text-danger"></i>`
+  `<i class="bi bi-suit-spade-fill ${redSwitcher()}"></i>`,
+  `<i class="bi bi-suit-club-fill ${redSwitcher()}"></i>`,
+  `<i class="bi bi-suit-diamond-fill ${redSwitcher()}"></i>`,
+  `<i class="bi bi-suit-heart-fill ${redSwitcher()}"></i>`
 ];
 
 const cardValue = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "K", "Q", "J", "A"];
@@ -26,12 +22,26 @@ window.onload = function() {
   cardValueHtml.innerHTML = randomCardValue(cardValue);
 };
 
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 const randomIcon = arr => {
-  let randomNumber = Math.floor(Math.random() * arr.length);
+  let randomNumber = getRandomIntInclusive(0, arr.length - 1);
   return arr[randomNumber];
 };
 
 const randomCardValue = arr => {
-  let randomNumber = Math.floor(Math.random() * arr.length);
+  let randomNumber = getRandomIntInclusive(0, arr.length - 1);
   return arr[randomNumber];
 };
+
+//Cambiar de color de los iconos
+//En arrow function no funciona llamarla primero y declararla después
+function redSwitcher() {
+  const num = getRandomIntInclusive(1, 100);
+  if (num < 50) return "text-danger";
+  return "";
+}
